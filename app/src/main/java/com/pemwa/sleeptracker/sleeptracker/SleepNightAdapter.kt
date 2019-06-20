69f906c595679696c33400f074b7e14ecfada2c0
petermwash <petermwash48@gmail.com>
2019-06-20 12:02:52 +0300

    Adding a recyclerView header

--- app/src/main/java/com/pemwa/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/pemwa/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -1,6 +1,7 @@
 package com.pemwa.sleeptracker.sleeptracker
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
@@ -10,24 +11,54 @@ import com.pemwa.sleeptracker.database.SleepNight
 import com.pemwa.sleeptracker.databinding.ListItemSleepNightBinding
 import com.pemwa.sleeptracker.util.convertDurationToFormatted
 import com.pemwa.sleeptracker.util.convertNumericQualityToString
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.ClassCastException
 
-class SleepNightAdapter(private val clickListener: SleepNightListener): ListAdapter<SleepNight, SleepNightAdapter.CustomViewHolder>(SleepNightDiffCallback()) {
-//    var data = listOf<SleepNight>()
-//        set(value) {
-//            field = value
-//            notifyDataSetChanged()
-//        }
-//
-//    override fun getItemCount() = data.size
+private val ITEM_VIEW_TYPE_HEADER = 0
+private val ITEM_VIEW_TYPE_ITEM = 1
 
-    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-//        val item = data[position]
-        val item = getItem(position)
-        holder.bind(clickListener,item)
+class SleepNightAdapter(private val clickListener: SleepNightListener): ListAdapter<DataItem, RecyclerView.ViewHolder>(SleepNightDiffCallback()) {
+
+    private val adapterScope = CoroutineScope(Dispatchers.Default)
+        fun addHeaderAndSubmitList(list: List<SleepNight>?) {
+            adapterScope.launch {
+                val items = when(list) {
+                    null -> listOf(DataItem.Header)
+                    else -> listOf(DataItem.Header) + list.map {
+                        DataItem.SleetNightItem(it)
+                    }
+                }
+                withContext(Dispatchers.Main) {
+                    submitList(items)
+                }
+            }
+        }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+            is CustomViewHolder -> {
+                val nightItem = getItem(position) as DataItem.SleetNightItem
+                holder.bind(clickListener, nightItem.sleepNight)
+            }
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        return CustomViewHolder.from(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            ITEM_VIEW_TYPE_HEADER -> TextViewHolder.from(parent)
+            ITEM_VIEW_TYPE_ITEM -> CustomViewHolder.from(parent)
+            else -> throw ClassCastException("Unknown viewType $viewType")
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is DataItem.Header -> ITEM_VIEW_TYPE_HEADER
+            is DataItem.SleetNightItem -> ITEM_VIEW_TYPE_ITEM
+        }
     }
 
     class CustomViewHolder private constructor(private val binding: ListItemSleepNightBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -51,16 +82,44 @@ class SleepNightAdapter(private val clickListener: SleepNightListener): ListAdap
     }
 }
 
-class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
-    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
-        return oldItem.nightId == newItem.nightId
+/**
+ * Callback for calculating the diff between two non-null items in a list.
+ *
+ * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
+ * list that's been passed to `submitList`.
+ */
+class SleepNightDiffCallback : DiffUtil.ItemCallback<DataItem>() {
+    override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+        return oldItem.id == newItem.id
     }
 
-    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+    override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
         return oldItem == newItem
     }
 }
 
 class SleepNightListener(val clickListener: (sleepId: Long) -> Unit) {
     fun onClick(night: SleepNight) = clickListener(night.nightId)
+}
+
+sealed class DataItem {
+    data class SleetNightItem(val sleepNight: SleepNight): DataItem() {
+        override val id = sleepNight.nightId
+    }
+
+    object Header: DataItem() {
+        override val id = Long.MIN_VALUE
+    }
+
+    abstract val id: Long
+}
+
+class TextViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    companion object {
+        fun from(parent: ViewGroup): TextViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(R.layout.header, parent, false)
+            return TextViewHolder(view)
+        }
+    }
 }
--- app/src/main/java/com/pemwa/sleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/pemwa/sleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -24,6 +23,9 @@ import com.pemwa.sleeptracker.databinding.FragmentSleepTrackerBinding
  */
 class SleepTrackerFragment : Fragment() {
 
+    lateinit var binding: FragmentSleepTrackerBinding
+    private lateinit var viewModel: SleepTrackerViewModel
+
     /**
      * Called when the Fragment is ready to display content to the screen.
      *
@@ -33,7 +35,7 @@ class SleepTrackerFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         // Getting a reference to the binding object and inflate the fragment views.
-        val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
+        binding  = DataBindingUtil.inflate(
             inflater, R.layout.fragment_sleep_tracker, container, false)
 
         // Getting a reference to application context
@@ -44,7 +46,7 @@ class SleepTrackerFragment : Fragment() {
 
         // Creating an instance of the ViewModelFactory and use it to get a reference to the SleepTrackerViewModel
         val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
-        val viewModel = ViewModelProviders.of(this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(SleepTrackerViewModel::class.java)
 
         // An observer to navigate to to SleepQualityFragment
         viewModel.navigateToSleepQuality.observe(this, Observer {  night ->
@@ -78,6 +80,22 @@ class SleepTrackerFragment : Fragment() {
             }
         })
 
+        getAdapter()
+
+        // Add the viewModel to data binding by setting the current UI-Controller as the lifecycle owner
+        // Then assigning the "sleepTrackerViewModel binding variable" to our sleepTrackerViewModel
+        binding.lifecycleOwner = this
+        binding.sleepTrackerViewModel = viewModel
+
+        getLayoutManager()
+
+        return binding.root
+    }
+
+    /**
+     * Associate our adapter with recyclerView and set the adapter data
+     */
+    private fun getAdapter() {
         // Associate our adapter with recyclerView
         val adapter = SleepNightAdapter(SleepNightListener { nightId ->
             viewModel.onSleepNightClicked(nightId)
@@ -87,20 +105,24 @@ class SleepTrackerFragment : Fragment() {
         // Observe nights on the viewModel and set the adapter data when there is any changes
         viewModel.nights.observe(viewLifecycleOwner, Observer {
             it.let { dataList ->
-//                adapter.data = it
-                adapter.submitList(dataList)
+                //                adapter.data = it
+                adapter.addHeaderAndSubmitList(dataList)
             }
-        }
-        )
+        })
+    }
 
-        // Add the viewModel to data binding by setting the current UI-Controller as the lifecycle owner
-        // Then assigning the "sleepTrackerViewModel binding variable" to our sleepTrackerViewModel
-        binding.lifecycleOwner = this
-        binding.sleepTrackerViewModel = viewModel
-
+    /**
+     * Define a layManager and specify the spanCount
+     */
+    private fun getLayoutManager() {
         val layoutManager = GridLayoutManager(activity, 3)
         binding.sleepList.layoutManager = layoutManager
 
-        return binding.root
+        layoutManager.spanSizeLookup = object: GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) = when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
     }
 }
